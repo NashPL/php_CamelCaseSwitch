48def7aad4a0734d9571075d8c9ba2f3fa6b3848
NashPL <nash25pl@gmail.com>
2019-06-26 13:32:42 +0100

    Added N type switcher function. Added comments to the class.

--- app.php
+++ app.php
@@ -7,3 +7,6 @@ require_once('src/camel/Camel.php');
 
 $text = new text\Camel($text);
 print_r($text->switchCamel());
+print("\n");
+print_r($text->nToCamel(5));
+print("\n");
--- src/camel/Camel.php
+++ src/camel/Camel.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace text;
+
+/**
+ * A class to transform a text character cases between lower and upper. 
+ */
 class Camel
 {
     private $originalText;
@@ -8,16 +12,35 @@ class Camel
         $this->originalText = $text;
     }
 
-    public function switchCamel()
+    /**
+     *  A function to switch text from upper to lower and vice versa
+     *  @return String A transformed text
+     */
+    public function switchCase() : string
     {
         $strArray = str_split($this->originalText);
         foreach ($strArray as $k => $v) {
-            if (ctype_upper($v)) {
-                $strArray[$k] = strtolower($v);
-            } else {
-                $strArray[$k] = strtoupper($v);
-            }
+            $strArray[$k] = (ctype_upper($v)) ? strtolower($v) : strtoupper($v);
+        }
+        return join($strArray);
+    }
 
+    /**
+     *  A function to switch text from upper to lower and vice versa but every n character in the string
+     *  @param Int a iteration of which n character has to be switched.
+     *  @return String A transformed text. 
+     * 
+     */
+    public function switchNToCase($n = 2) : string
+    {
+        if($n === 0) { return $this->originalText; } // If N is 0 then we do not transform
+        $strArray = str_split($this->originalText);
+        array_unshift($strArray, "");
+        unset($strArray[0]);
+        for ($i = 0; $i <= count($strArray); $i = $i + $n) {
+            if(!empty($strArray[$i])){
+                $strArray[$i] = (ctype_upper($strArray[$i])) ? strtolower($strArray[$i]) : strtoupper($strArray[$i]);
+            }
         }
         return join($strArray);
     }
